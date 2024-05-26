dc7d6d607abdcda532b6e06b991c5f2cbdb90568
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-05-26 10:07:04 +0200

    260520241

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,24 +1,26 @@
-// import { deleteContact } from 'components/redux/contactsSlice';
-// import { getContacts, getFilter } from 'components/redux/state';
+import { deleteContact } from 'components/redux/operations';
+import { getContacts, getFilter } from 'components/redux/state';
+
+import { useDispatch, useSelector } from 'react-redux';
 import css from './ContactList.module.css';
 
 export const ContactList = () => {
-  // const contacts = useSelector(getContacts);
-  // const filter = useSelector(getFilter);
-  // const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
 
-  // const handleDelete = e => {
-  //   dispatch(deleteContact(e.target.id));
-  // };
+  const handleDelete = e => {
+    dispatch(deleteContact(e.target.id));
+  };
 
   return (
     <ul className={css.list}>
-      {/* {contacts
+      {contacts
         .filter(contact =>
           contact.name.toLowerCase().includes(filter.toLowerCase())
         )
-        .map(contact => { */}
-      {/* return (
+        .map(contact => {
+          return (
             <li key={contact.id}>
               {contact.name} : {contact.number}{' '}
               <button
@@ -30,7 +32,7 @@ export const ContactList = () => {
               </button>
             </li>
           );
-        })} */}
+        })}
     </ul>
   );
 };
